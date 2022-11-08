36c6315112f4019f07fca6b49fd7f2f2a2bb2a57
KGD <kimgyudo93@gmail.com>
2022-11-08 20:45:50 +0900

    new Map

---
+++ MapProject/TestMap/app/src/main/java/com/example/testmap/GmapViewFragment.kt
@@ -0,0 +1,93 @@
+package com.example.testmap
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+
+class GmapViewFragment : Fragment(), OnMapReadyCallback {
+
+
+    lateinit var mContext: Context
+    private lateinit var mView: MapView
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context is MapsActivity) {
+            mContext = context
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var rootView = inflater.inflate(R.layout.fragment_gmap_view, container, false)
+        mView = rootView.findViewById(R.id.mapView)
+        mView.onCreate(savedInstanceState)
+        mView.getMapAsync(this)
+
+        return rootView
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val marker = LatLng(37.568291,126.997780)
+        googleMap.addMarker(MarkerOptions().position(marker).title("여기"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15f))
+
+        val sydney = LatLng(-33.852, 151.211)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
+
+        val seoul = LatLng(33.981166667, 58.677166667)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(seoul)
+                .title("Desk")
+        )
+        
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mView.onStart()
+    }
+    override fun onStop() {
+        super.onStop()
+        mView.onStop()
+    }
+    override fun onResume() {
+        super.onResume()
+        mView.onResume()
+    }
+    override fun onPause() {
+        super.onPause()
+        mView.onPause()
+    }
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mView.onLowMemory()
+    }
+    override fun onDestroy() {
+        mView.onDestroy()
+        super.onDestroy()
+    }
+
+}
---
+++ MapProject/TestMap/app/src/main/java/com/example/testmap/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.testmap
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+
+
+            finish()
+        }, 2000)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
